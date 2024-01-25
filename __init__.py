c2e2078521f48e7f27826d03bf7333cb9edb7c36
Nicholas Gower <nicholasgower01@gmail.com>
2024-01-25 11:18:43 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-import * from abctools #When this submodule is imported, the root directory(this) shall be equivalent to the abctools directory, which contains the actual django app.
+import * from .abctools #When this submodule is imported, the root directory(this) shall be equivalent to the abctools directory, which contains the actual django app.
